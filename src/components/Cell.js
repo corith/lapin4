35dac36682bb1022b1d07b200b5948aec0815cf8
Cory Sebastian <sebastiancory@yahoo.com>
2021-12-17 13:03:25 -0500

    Cleaned up the winner detection code. fixed some minor bugs

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,17 +1,15 @@
 import '../App.css'
-import {useEffect, useState} from "react";
 
 export const Cell = ({playerNumber , cIndex, takeTurn}) => {
-    const [owner , setOwner] = useState(0)
-
     let color1 = 'bg-gradient-to-r from-blue-700 via-blue-400 to-blue-700'
+    let color1W = 'bg-gradient-to-r from-blue-700 via-blue-400 to-blue-700 border border-green-400'
     let color2 = 'bg-gradient-to-r from-yellow-600 via-yellow-400 to-yellow-600'
+    let color2W = 'bg-gradient-to-r from-yellow-600 via-yellow-400 to-yellow-600 border border-green-400'
 
     return (
         <div className='gameCell rounded flex justify-center items-center' onClick={(e) => {takeTurn(cIndex)}}>
             <div className={(playerNumber === 0 ? color1 : playerNumber === 2 ? 'bg-gray-400' : playerNumber === 3 ? 'bg-red-600' : color2)
                 + ' rounded-full h-3/4 w-3/4'}>
-
             </div>
         </div>
     )
--- src/components/GameBoardComponent.js
+++ src/components/GameBoardComponent.js
@@ -1,89 +1,114 @@
 import {Cell} from "./Cell";
-import {useEffect} from "react";
+import {useState} from "react";
 
-export const GameBoardComponent = ({isLive, whoseTurn, setTurn, board}) => {
+export const GameBoardComponent = ({isLive, whoseTurn, setTurn, board, p1, p2, thereIsAWinner, setWinner}) => {
 
     const takeTurn = (cIndex) => {
-        if (isLive) {
-            for (let i = 5; i < board.length; i--) {
+        if (isLive && !thereIsAWinner) {
+            for (let i = 5; i >= 0; i--) {
                 if (board[i][cIndex] == null) {
                     board[i][cIndex] = whoseTurn === 1 ? 1 : 2
+                    if (!checkForWinner()) {
+                        setTurn(whoseTurn === 1 ? 2 : 1)
+                    }
+                    console.log(board)
                     break
                 }
             }
-            setTurn(whoseTurn === 1 ? 2 : 1)
+        } else if (thereIsAWinner) {
+            alert("There has already been a winner! " + (whoseTurn === 1 ? p1.name : p2.name)  + " won!")
         }
-        console.log("Checking for winner")
-        checkForWinner()
     }
 
     const checkForWinner = () => {
-        console.log(board)
-        checkPlayer(1)
-        checkPlayer(2)
+        if (checkPlayer(1)) {
+            highlightWinners(1)
+            return true
+        } else if (checkPlayer(2)) {
+            highlightWinners(2)
+            return true
+        }
+        return false
+    }
 
+    const checkHoriz = (i,z,num) => {
+        let connected = 1
+        for (let x = 1; x < 4; x++) {
+            if (board[i][z+x] === num) {
+                console.log("counting a horizontal")
+                connected += 1
+            }
+        }
+        return connected
+    }
+
+    const checkVert = (i,z,num) => {
+        let connected = 1
+        for (let x = 1; x < 4; x++) {
+            if (i < 3 && board[i+x][z] === num) {
+                connected += 1
+            }
+        }
+        return connected
+    }
+
+    const checkDiag = (i,z,num) => {
+        let connected = 1
+        // Check Diagnol right
+        for (let x = 1; x < 4; x++) {
+            if (z < 5 && i < 3 && board[i+x][z+x] === num) {
+                connected += 1
+            }
+        }
+
+        // Check Diagnol left
+        if (connected !== 4) {
+            connected = 1
+        } else {
+            return connected
+        }
+
+        for (let x = 1; x < 4; x++) {
+            if ( i < 3 && board[i+x][z-x] === num) {
+                connected += 1
+            }
+        }
+        return connected
+    }
+
+    const checkStaleMate = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let z = 0; z < board[i].length; z++) {
+                if (board[i][z] === null) {
+                    return false
+                }
+            }
+        }
+        alert("There has been a stalemate")
+        return true
+    }
+
+    const highlightWinners = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let z = 0; z < board[i].length; z++) {
+                if (board[i][z] === null) {
+                    return false
+                }
+            }
+        }
     }
 
     const checkPlayer = (num) => {
         for (let i = 0; i < board.length; i++) {
             for(let z = 0; z < board[i].length; z++) {
                 if(board[i][z] === num) {
-                    let connected = 1
-                    //check horizontal ->
-                    for (let x = 1; x < 4; x++) {
-                        if (board[i][z+x] === num) {
-                            connected += 1
-                        }
-                    }
-                    if (connected === 4) {
-                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
-                        break
-                    } else {
-                        connected = 1
-                    }
-
-                    // Check Vertical
-                    for (let x = 1; x < 4; x++) {
-                        if (i < 3 && board[i+x][z] === num) {
-                            connected += 1
-                        }
-                    }
+                    let connected = checkHoriz(i, z, num) === 4 ? 4 : checkVert(i,z,num) === 4 ? 4 : checkDiag(i,z,num)
 
                     if (connected === 4) {
-                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
-                        break
-                    } else {
-                        connected = 1
+                        alert((num === 1 ? p1.name : p2.name) + " wins")
+                        setWinner(true)
+                        return true
                     }
-
-                    // Check Diagnol right
-                    for (let x = 1; x < 4; x++) {
-                        if (z < 5 && i < 3 && board[i+x][z+x] === num) {
-                            connected += 1
-                        }
-                    }
-
-                    if (connected === 4) {
-                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
-                        break
-                    } else {
-                        connected = 1
-                    }
-
-                    // Check Diagnol left
-                    for (let x = 1; x < 4; x++) {
-                        if ( i < 3 && board[i+x][z-x] === num) {
-                            connected += 1
-                        }
-                    }
-
-                    if (connected === 4) {
-                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
-                        break
-                    } else {
-                        connected = 1
-                    }
-
                 }
             }
         }
