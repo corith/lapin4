8b5a4d47cab6ea10cf9cb35504167ffe8751886c
Cory Sebastian <sebastiancory@yahoo.com>
2021-12-17 10:40:21 -0500

    Brute force solution to winner detection lol

--- src/components/GameBoardComponent.js
+++ src/components/GameBoardComponent.js
@@ -13,7 +13,82 @@ export const GameBoardComponent = ({isLive, whoseTurn, setTurn, board}) => {
             }
             setTurn(whoseTurn === 1 ? 2 : 1)
         }
+        console.log("Checking for winner")
+        checkForWinner()
     }
+
+    const checkForWinner = () => {
+        console.log(board)
+        checkPlayer(1)
+        checkPlayer(2)
+
+    }
+
+    const checkPlayer = (num) => {
+        for (let i = 0; i < board.length; i++) {
+            for(let z = 0; z < board[i].length; z++) {
+                if(board[i][z] === num) {
+                    let connected = 1
+                    //check horizontal ->
+                    for (let x = 1; x < 4; x++) {
+                        if (board[i][z+x] === num) {
+                            connected += 1
+                        }
+                    }
+                    if (connected === 4) {
+                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
+                        break
+                    } else {
+                        connected = 1
+                    }
+
+                    // Check Vertical
+                    for (let x = 1; x < 4; x++) {
+                        if (i < 3 && board[i+x][z] === num) {
+                            connected += 1
+                        }
+                    }
+
+                    if (connected === 4) {
+                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
+                        break
+                    } else {
+                        connected = 1
+                    }
+
+                    // Check Diagnol right
+                    for (let x = 1; x < 4; x++) {
+                        if (z < 5 && i < 3 && board[i+x][z+x] === num) {
+                            connected += 1
+                        }
+                    }
+
+                    if (connected === 4) {
+                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
+                        break
+                    } else {
+                        connected = 1
+                    }
+
+                    // Check Diagnol left
+                    for (let x = 1; x < 4; x++) {
+                        if ( i < 3 && board[i+x][z-x] === num) {
+                            connected += 1
+                        }
+                    }
+
+                    if (connected === 4) {
+                        alert((num === 1 ? "Player 1" : "Player 2") + " wins")
+                        break
+                    } else {
+                        connected = 1
+                    }
+
+                }
+            }
+        }
+    }
+
 
     return (
         <table className='table-auto flex justify-center pt-5'>
@@ -26,7 +101,6 @@ export const GameBoardComponent = ({isLive, whoseTurn, setTurn, board}) => {
                                 [...Array(8)].map((e2 , z) => {
                                     return (
                                         <td key={z}>
-                                            {/*<Cell cIndex={z} playerNumber={whoseTurn === 1 ? 1 : 2} takeTurn={takeTurn}/>*/}
                                             <Cell cIndex={z} playerNumber={row[z] === 1 ? 0 : row[z] === 2 ? 1 : row[z] === 3 ? 3 : 2} takeTurn={takeTurn}/>
                                         </td>
                                     )
